53eab8adc9052a807ffad19e296fbb7ce4bc672d
iLLRaps3 <140134684+iLLRaps3@users.noreply.github.com>
2025-07-18 22:30:12 -0700

    Add server-bot-quick-start/prompt_bot.py 1752903015103

---
+++ server-bot-quick-start/prompt_bot.py
@@ -0,0 +1,67 @@
+"""
+
+Sample bot that wraps Claude-3-Haiku but makes responses Haikus
+
+"""
+
+from __future__ import annotations
+
+import os
+from typing import AsyncIterable
+
+import fastapi_poe as fp
+from modal import App, Image, asgi_app
+
+SYSTEM_PROMPT = """
+All your replies are Haikus.
+""".strip()
+
+# TODO: set your bot access key and bot name for this bot to work
+# see https://creator.poe.com/docs/quick-start#configuring-the-access-credentials
+bot_access_key = "5ng4GbHhoAHMaXyWxuQIeSzCE97MDq7q"
+bot_name = "BotSY31WFXM56"
+
+
+class PromptBot(fp.PoeBot):
+    async def get_response(
+        self, request: fp.QueryRequest
+    ) -> AsyncIterable[fp.PartialResponse]:
+        request.query = [
+            fp.ProtocolMessage(role="system", content=SYSTEM_PROMPT)
+        ] + request.query
+        async for msg in fp.stream_request(
+            request, "Claude-3-Haiku", request.access_key
+        ):
+            yield msg
+
+    def get_settings(self, setting: fp.SettingsRequest) -> fp.SettingsResponse:
+        return fp.SettingsResponse(server_bot_dependencies={"Claude-3-Haiku": 1})
+
+
+REQUIREMENTS = ["fastapi-poe"]
+image = (
+    Image.debian_slim()
+    .pip_install(*REQUIREMENTS)
+    .env({"POE_ACCESS_KEY": bot_access_key})
+)
+app = App("prompt-bot-poe")
+
+
+@app.function(image=image)
+@asgi_app()
+def fastapi_app():
+    bot = PromptBot()
+    app_instance = fp.make_app(
+        bot,
+        access_key=bot_access_key,
+        bot_name=bot_name,
+        allow_without_key=not (bot_access_key and bot_name),
+    )
+    return app_instance
+
+app_instance = fp.make_app(
+    PromptBot(),
+    access_key=bot_access_key,
+    bot_name=bot_name,
+    allow_without_key=not (bot_access_key and bot_name),
+)
